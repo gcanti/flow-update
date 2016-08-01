659efc6f216a5b50fefd76753e5c66943f2d9ad5
gcanti <giulio.canti@gmail.com>
2016-08-01 09:47:22 +0200

    fix spliceArray signature

--- flow-update.js
+++ flow-update.js
@@ -1,18 +1,18 @@
 declare module 'flow-update' {
 
-  declare function mergeObject<T: Object>(obj: T, fields: $Shape<T>): T;
-
-  declare function mergeArray<T>(arr: Array<T>, indexes: Array<number>, values: Array<T>): Array<T>;
-
-  declare function swapArray<T>(arr: Array<T>, from: number, to: number): Array<T>;
-
   declare type Splice<T> = {
     start: number,
     deleteCount: number,
     items?: Array<T>
   };
 
-  declare function spliceArray<T>(arr: Array<T>, splices: Array<Splice<T>>);
+  declare function mergeObject<T: Object>(obj: T, fields: $Shape<T>): T;
+
+  declare function mergeArray<T>(arr: Array<T>, indexes: Array<number>, values: Array<T>): Array<T>;
+
+  declare function swapArray<T>(arr: Array<T>, from: number, to: number): Array<T>;
+
+  declare function spliceArray<T>(arr: Array<T>, splices: Array<Splice<T>>): Array<T>;
 
   declare function mergeDictionary<K, V>(dict: {[key: K]: V}, fields: {[key: K]: V}): {[key: K]: V};
 
